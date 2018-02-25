5795b40f3e5f9f4a42e84776a3b44c1d80963eb6
David Mrnustik <david.mrnustik@hotmail.com>
2018-02-25 19:53:42 +0100

    app.json, modify styles, state app before build

--- App.js
+++ App.js
@@ -98,9 +98,5 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginLeft: 10,
-    marginRight: 10,
-    // alignItems: 'center',
-    // justifyContent: 'center'
   }
 })
